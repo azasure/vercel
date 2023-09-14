643431c2bda107321b2ff353cebcd184f6feaf4f
azasure <140221273+azasure@users.noreply.github.com>
2023-09-14 01:39:03 -0400

    Update App.js

--- examples/create-react-app/src/App.js
+++ examples/create-react-app/src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import HttpError from '@wasp/core/HttpError.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export const tradeCardOrCryptocurrency = async (args, context) => {
+  if (!context.user) { throw new HttpError(401) };
+
+  // Implement the trade logic here
 }
 
-export default App;
+export const withdrawFunds = async (args, context) => {
+  if (!context.user) { throw new HttpError(401) };
+
+  const { userId, amount } = args;
+
+  const user = await context.entities.User.findUnique({
+    where: { id: userId }
+  });
+
+  if (!user) { throw new HttpError(404) };
+
+  const wallet = await context.entities.Wallet.findUnique({
+    where: { userId: user.id }
+  });
+
+  if (!wallet) { throw new HttpError(404) };
+
+  if (wallet.balance < amount) { throw new HttpError(400, 'Insufficient funds') };
+
+  const transaction = await context.entities.Transaction.create({
+    data: {
+      type: 'withdrawal',
+      details: `Withdrawal from wallet to bank account`,
+      status: 'pending',
+      user: { connect: { id: user.id } }
+    }
+  });
+
+  const updatedWallet = await context.entities.Wallet.update({
+    where: { id: wallet.id },
+    data: { balance: wallet.balance - amount }
+  });
+
+  return transaction;
+}
